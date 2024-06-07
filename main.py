80ac0449f17c4e1fbde8146c3f1eba61afa9cb86
ezgi <ezgiogulmus@gmail.com>
2024-06-07 10:54:49 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,282 @@
+from __future__ import print_function
+import argparse
+
+import os
+import sys
+import json
+from timeit import default_timer as timer
+import numpy as np
+import pandas as pd
+import torch
+import wandb
+
+### Internal Imports
+from datasets.dataset_survival import MIL_Survival_Dataset
+from utils.file_utils import save_pkl, load_pkl
+from utils.core_utils import train
+from utils.utils import check_directories, get_data
+
+device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def main(args=None):
+	if args is None:
+		args = setup_argparse()
+	seed_torch(args.seed)
+
+	args = check_directories(args)
+		
+	if args.wandb:
+		args.k = 1
+		args.max_epochs = 20
+		args.early_stopping = 10
+		wandb.init(config=vars(args))
+		config = wandb.config
+		for key, value in config.items():
+			print(key, value)
+			setattr(args, key, value)
+		
+		args.run_name = wandb.run.name
+		args.results_dir = os.path.join(args.results_dir, args.data_name, "wandb", args.run_name)
+	
+	os.makedirs(args.results_dir, exist_ok=True)
+	if ('summary_latest.csv' in os.listdir(args.results_dir)) and (not args.overwrite):
+		print("Exp Code <%s> already exists! Exiting script." % args.run_name)
+		sys.exit()
+
+	settings = vars(args)
+	print("Saving to ", args.results_dir)
+	with open(args.results_dir + '/experiment.json', 'w') as f:
+		json.dump(settings, f, indent=4)
+	
+	print("################# Settings ###################")
+	for key, val in settings.items():
+		print("{}:  {}".format(key, val)) 
+
+	print("Loading all the data ...")
+	df, indep_vars = get_data(args)
+	dataset = MIL_Survival_Dataset(
+		df=df,
+		data_dir=args.feats_dir,
+		mode= args.mode,
+		sign_path=os.path.join(args.dataset_dir, "signatures.csv"),
+		print_info=True,
+		n_bins=args.n_classes,
+		indep_vars=indep_vars
+	)
+
+	if args.k_start == -1:
+		start = 0
+	else:
+		start = args.k_start
+	if args.k_end == -1:
+		end = args.k
+	else:
+		end = args.k_end
+
+	results = None
+	folds = np.arange(start, end)
+
+	### Start 5-Fold CV Evaluation.
+	for i in folds:
+		start = timer()
+		seed_torch(args.seed)
+		val_results_pkl_path = os.path.join(args.results_dir, 'latest_val_results_split{}.pkl'.format(i))
+		test_results_pkl_path = os.path.join(args.results_dir, 'latest_test_results_split{}.pkl'.format(i))
+		if os.path.isfile(test_results_pkl_path):
+			print("Skipping Split %d" % i)
+			continue
+
+		### Gets the Train + Val Dataset Loader.
+		datasets, train_stats = dataset.return_splits(os.path.join(args.split_dir, f"splits_{i}.csv"))
+		train_stats.to_csv(os.path.join(args.results_dir, f'train_stats_{i}.csv'))
+		
+		log, val_latest, test_latest = train(datasets, i, args)
+		
+		if results is None:
+			results = {k: [] for k in log.keys()}
+		
+		for k in log.keys():
+			results[k].append(log[k])
+		
+		if args.wandb:
+			wandb.log(log)
+
+		save_pkl(val_results_pkl_path, val_latest)
+		if test_latest != None:
+			save_pkl(test_results_pkl_path, test_latest)
+		end = timer()
+		print('Fold %d Time: %f seconds' % (i, end - start))
+	
+		pd.DataFrame(results).to_csv(os.path.join(args.results_dir, 'summary_latest.csv'))
+
+
+def setup_argparse():
+	### Data 
+	parser = argparse.ArgumentParser(description='Configurations for Survival Analysis on TCGA Data.')
+	parser.add_argument('--run_name',      type=str, default='run')
+	parser.add_argument('--data_name',   type=str, default=None)
+	parser.add_argument('--feats_dir',   type=str, default=None)
+
+	parser.add_argument('--dataset_dir', type=str, default="./datasets_csv")
+	parser.add_argument('--results_dir',     type=str, default='./results', help='Results directory (Default: ./results)')
+	parser.add_argument('--split_dir',       type=str, default="./splits", help='Split directory (Default: ./splits)')
+
+	parser.add_argument('--run_config_file',      type=str, default=None)
+	
+	### Experiment
+	parser.add_argument('--wandb',		 action='store_true', default=False)
+	parser.add_argument('--seed', 			 type=int, default=1, help='Random seed for reproducible experiment (default: 1)')
+	parser.add_argument('--k', 			     type=int, default=5, help='Number of folds (default: 5)')
+	parser.add_argument('--k_start',		 type=int, default=-1, help='Start fold (Default: -1, last fold)')
+	parser.add_argument('--k_end',			 type=int, default=-1, help='End fold (Default: -1, first fold)')
+	parser.add_argument('--log_data',        action='store_true', default=True, help='Log data using tensorboard')
+	parser.add_argument('--overwrite',     	 action='store_true', default=False, help='Whether or not to overwrite experiments (if already ran)')
+
+	### Model Parameters.
+	parser.add_argument('--omics', default=None)
+	parser.add_argument('--selected_features',     	 action='store_false', default=True)
+	parser.add_argument('--n_classes', type=int, default=4)
+
+	parser.add_argument('--model_type',      type=str, choices=['snn', 'deepset', 'amil', 'mi_fcn', 'mcat'], default='mcat', help='Type of model (Default: mcat)')
+	parser.add_argument('--mode',            type=str, choices=['omic', 'path', 'pathomic', 'cluster', 'coattn'], default='coattn', help='Specifies which modalities to use / collate function in dataloader.')
+	parser.add_argument('--fusion',          type=str, choices=['None', 'concat', 'bilinear'], default='concat', help='Type of fusion. (Default: concat).')
+	parser.add_argument('--apply_sig',		 action='store_true', default=False, help='Use genomic features as signature embeddings.')
+	parser.add_argument('--apply_sigfeats',  action='store_true', default=False, help='Use genomic features as tabular features.')
+	parser.add_argument('--drop_out',        action='store_true', default=True, help='Enable dropout (p=0.25)')
+	parser.add_argument('--model_size_wsi',  type=str, default='small', help='Network size of AMIL model')
+	parser.add_argument('--model_size_omic', type=str, default='small', help='Network size of SNN model')
+
+	### Optimizer Parameters + Survival Loss Function
+	parser.add_argument('--opt',             type=str, choices = ['adam', 'sgd'], default='adam')
+	parser.add_argument('--batch_size',      type=int, default=1, help='Batch Size (Default: 1, due to varying bag sizes)')
+	parser.add_argument('--gc',              type=int, default=32, help='Gradient Accumulation Step.')
+	parser.add_argument('--max_epochs',      type=int, default=20, help='Maximum number of epochs to train (default: 20)')
+	parser.add_argument('--lr',				 type=float, default=2e-4, help='Learning rate (default: 0.0001)')
+	parser.add_argument('--bag_loss',        type=str, choices=['svm', 'ce', 'ce_surv', 'nll_surv', 'cox_surv'], default='nll_surv', help='slide-level classification loss function (default: ce)')
+	parser.add_argument('--label_frac',      type=float, default=1.0, help='fraction of training labels (default: 1.0)')
+	parser.add_argument('--bag_weight',      type=float, default=0.7, help='clam: weight coefficient for bag-level loss (default: 0.7)')
+	parser.add_argument('--reg', 			 type=float, default=1e-5, help='L2-regularization weight decay (default: 1e-5)')
+	parser.add_argument('--alpha_surv',      type=float, default=0.0, help='How much to weigh uncensored patients')
+	parser.add_argument('--reg_type',        type=str, choices=['None', 'omic', 'pathomic'], default='None', help='Which network submodules to apply L1-Regularization (default: None)')
+	parser.add_argument('--lambda_reg',      type=float, default=1e-4, help='L1-Regularization Strength (Default 1e-4)')
+	parser.add_argument('--weighted_sample', action='store_true', default=True, help='Enable weighted sampling')
+	parser.add_argument('--early_stopping',  action='store_true', default=False, help='Enable early stopping')
+
+
+	args = parser.parse_args()
+	return args
+
+
+### Sets Seed for reproducible experiments.
+def seed_torch(seed=7):
+	import random
+	random.seed(seed)
+	os.environ['PYTHONHASHSEED'] = str(seed)
+	np.random.seed(seed)
+	torch.manual_seed(seed)
+	if device.type == 'cuda':
+		torch.cuda.manual_seed(seed)
+		torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
+	torch.backends.cudnn.benchmark = False
+	torch.backends.cudnn.deterministic = True
+
+			
+
+if __name__ == "__main__":
+	args = setup_argparse()
+	if args.run_config_file:
+		new_run_name = args.run_name
+		results_dir = args.results_dir
+		feats_dir = args.feats_dir
+		wandb = args.wandb
+		cv_fold = args.k
+		max_epochs = args.max_epochs
+		with open(args.run_config_file, "r") as f:
+			config = json.load(f)
+		
+		parser = argparse.ArgumentParser()
+		parser.add_argument("--run_config_file")
+		for k, v in config.items():
+			if k != "run_config_file":
+				parser.add_argument('--' + k, default=v, type=type(v))
+		args = parser.parse_args()
+		args.run_name = new_run_name
+		args.feats_dir = feats_dir
+		args.results_dir = results_dir
+		args.wandb = wandb
+		args.k = cv_fold
+		args.max_epochs = max_epochs
+		args.split_dir = args.split_dir.split("/")[-1]
+		start = timer()
+		results = main(args)
+		end = timer()
+		print("finished!")
+		print("end script")
+		print('Script Time: %f seconds' % (end - start))
+	elif not args.wandb:
+		start = timer()
+		results = main()
+		end = timer()
+		print("finished!")
+		print("end script")
+		print('Script Time: %f seconds' % (end - start))
+	else:
+		
+		parameter_dict = {
+			"opt": {
+				# "values": ["sgd", "adam"]
+				"value": "adam"
+			},
+			"lr":{
+				"values": [1e-5, 1e-4, 1e-3]
+				# "value": 1e-3
+			},
+			"reg":{
+				"values": [1e-2, 1e-4, 1e-3]
+			},
+			# 'drop_out': {
+			# 	"values": [.25, .50, .75]
+			# },
+			# "gc": {
+			# 	"values": [32, 64, 128]
+			# },
+			
+		# }
+		# if args.feats_dir is not None:
+		# 	parameter_dict.update({
+				'model_dim': {
+					# "values": [None, 128, 256]
+					'value': None
+				},
+				'depth': {
+					"values": [3, 5]
+				},
+				'mha_heads': {
+					"values": [4, 6]
+				},
+				'dim_head': {
+					"values": [16, 32]
+				},
+		# 	})
+		# else:
+		# 	parameter_dict.update({
+				"mlp_type": {
+					"values": ["tiny", "big"]
+				},
+				# "activation": {
+				# 	"values": ["relu", "leakyrelu", "gelu"]
+				# },
+				# "mlp_depth": {
+				# 	"values": [3, 5, 7]
+				# }
+			}
+		sweep_config = {
+			'method': 'random',
+			'metric': {
+				'name': 'test_cindex',
+				'goal': 'maximize'
+			},
+			'parameters': parameter_dict
+		}
+		sweep_id = wandb.sweep(sweep_config, project=args.run_name+"_"+args.split_dir) 
+		wandb.agent(sweep_id, function=main)
---
+++ utils/core_utils.py
@@ -0,0 +1,261 @@
+from argparse import Namespace
+import os
+import numpy as np
+from sksurv.metrics import concordance_index_censored
+import torch
+
+from datasets.dataset_generic import save_splits
+from models.model_genomic import SNN
+from models.model_set_mil import MIL_Sum_FC_surv, MIL_Attention_FC_surv, MIL_Cluster_FC_surv
+from models.model_coattn import MCAT_Surv
+from utils.utils import *
+
+from utils.coattn_train_utils import *
+from utils.cluster_train_utils import *
+
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self, warmup=5, patience=15, stop_epoch=20, verbose=False):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 20
+            stop_epoch (int): Earliest epoch possible for stopping
+            verbose (bool): If True, prints a message for each validation loss improvement. 
+                            Default: False
+        """
+        self.warmup = warmup
+        self.patience = patience
+        self.stop_epoch = stop_epoch
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+
+    def __call__(self, epoch, val_loss, model, ckpt_name = 'checkpoint.pt'):
+
+        score = -val_loss
+
+        if epoch < self.warmup:
+            pass
+        elif self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model, ckpt_name)
+        elif score < self.best_score:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience and epoch > self.stop_epoch:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model, ckpt_name)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model, ckpt_name):
+        '''Saves model when validation loss decrease.'''
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), ckpt_name)
+        self.val_loss_min = val_loss
+
+
+def train(datasets: tuple, cur: int, args: Namespace):
+    """   
+        train for a single fold
+    """
+    print('\nTraining Fold {}!'.format(cur))
+    writer_dir = os.path.join(args.results_dir, str(cur))
+    if not os.path.isdir(writer_dir):
+        os.mkdir(writer_dir)
+
+    if args.log_data:
+        from tensorboardX import SummaryWriter
+        writer = SummaryWriter(writer_dir, flush_secs=15)
+
+    else:
+        writer = None
+
+    print('\nInit train/val/test splits...', end=' ')
+    train_split, val_split, test_split = datasets
+    train_survival = np.array(list(zip(train_split.slide_data["censorship"].values, train_split.slide_data["survival_months"].values)), dtype=[('censorship', bool), ('time', np.float64)])
+    max_surv_limit = int(np.min([train_split.slide_data["survival_months"].max(), val_split.slide_data["survival_months"].max(), test_split.slide_data["survival_months"].max()]))
+    save_splits(datasets, ['train', 'val', 'test'], os.path.join(args.results_dir, 'splits_{}.csv'.format(cur)))
+    print('Done!')
+    print("Training on {} samples".format(len(train_split)))
+    print("Validating on {} samples".format(len(val_split)))
+    print("Testing on {} samples".format(len(test_split)))
+
+    print('\nInit loss function...', end=' ')
+    if args.bag_loss == 'ce_surv':
+        loss_fn = CrossEntropySurvLoss(alpha=args.alpha_surv)
+    elif args.bag_loss == 'nll_surv':
+        loss_fn = NLLSurvLoss(alpha=args.alpha_surv)
+    elif args.bag_loss == 'cox_surv':
+        loss_fn = CoxSurvLoss()
+    else:
+        raise NotImplementedError
+
+    if args.reg_type == 'omic':
+        reg_fn = l1_reg_all
+    elif args.reg_type == 'pathomic':
+        reg_fn = l1_reg_modules
+    else:
+        reg_fn = None
+
+    print('Done!')
+    
+    print('\nInit Model...', end=' ')
+    model_dict = {"dropout": args.drop_out, 'n_classes': args.n_classes}
+    args.fusion = None if args.fusion == 'None' else args.fusion
+
+    if args.model_type =='snn':
+        model_dict = {'omic_input_dim': args.omic_input_dim, 'model_size_omic': args.model_size_omic, 'n_classes': args.n_classes}
+        model = SNN(**model_dict)
+    elif args.model_type == 'deepset':
+        model_dict = {'omic_input_dim': args.omic_input_dim, 'fusion': args.fusion, 'n_classes': args.n_classes}
+        model = MIL_Sum_FC_surv(**model_dict)
+    elif args.model_type =='amil':
+        model_dict = {'omic_input_dim': args.omic_input_dim, 'fusion': args.fusion, 'n_classes': args.n_classes}
+        model = MIL_Attention_FC_surv(**model_dict)
+    elif args.model_type == 'mi_fcn':
+        model_dict = {'omic_input_dim': args.omic_input_dim, 'fusion': args.fusion, 'num_clusters': 10, 'n_classes': args.n_classes}
+        model = MIL_Cluster_FC_surv(**model_dict)
+    elif args.model_type == 'mcat':
+        model_dict = {"path_input_dim": args.path_input_dim, 'fusion': args.fusion, 'omic_sizes': args.omic_sizes, 'n_classes': args.n_classes}
+        model = MCAT_Surv(**model_dict)
+    else:
+        raise NotImplementedError
+    
+    if hasattr(model, "relocate"):
+        model.relocate()
+    else:
+        model = model.to(torch.device('cuda'))
+    print('Done!')
+    print_network(model)
+
+    print('\nInit optimizer ...', end=' ')
+    optimizer = get_optim(model, args)
+    print('Done!')
+    
+    print('\nInit Loaders...', end=' ')
+    train_loader = get_split_loader(train_split, training=True, weighted = args.weighted_sample, mode=args.mode, batch_size=args.batch_size)
+    val_loader = get_split_loader(val_split, mode=args.mode, batch_size=args.batch_size)
+    test_loader = get_split_loader(test_split, batch_size=args.batch_size)
+    print('Done!')
+
+    print('\nSetup EarlyStopping...', end=' ')
+    if args.early_stopping:
+        early_stopping = EarlyStopping(warmup=0, patience=10, stop_epoch=20, verbose = True)
+    else:
+        early_stopping = None
+
+    for epoch in range(args.max_epochs):
+        loop_survival(cur, epoch, model, train_loader, loss_fn, reg_fn, args.lambda_reg, writer, optimizer, args.gc, coattn=True if args.mode == "coattn" else False)
+        stop = loop_survival(cur, epoch, model, val_loader, loss_fn, reg_fn, args.lambda_reg, writer, coattn=True if args.mode == "coattn" else False, training=False, results_dir=args.results_dir, early_stopping=early_stopping)
+        if stop:
+            break
+    
+    torch.save(model.state_dict(), os.path.join(args.results_dir, "s_{}_checkpoint.pt".format(cur)))
+    if os.path.isfile(os.path.join(args.results_dir, "s_{}_minloss_checkpoint.pt".format(cur))):
+        model.load_state_dict(torch.load(os.path.join(args.results_dir, "s_{}_minloss_checkpoint.pt".format(cur))))
+    
+    results_val_dict, val_cindex = loop_survival(cur, epoch, model, val_loader, loss_fn, reg_fn, args.lambda_reg, coattn=True if args.mode == "coattn" else False, training=False, return_summary=True)
+    results_test_dict, test_cindex = loop_survival(cur, epoch, model, test_loader, loss_fn, reg_fn, args.lambda_reg, coattn=True if args.mode == "coattn" else False, training=False, return_summary=True)
+    print('Val c-Index: {:.4f} | Test c-Index: {:.4f}'.format(val_cindex, test_cindex))
+    if writer:
+        writer.add_scalar('latest_val/c_index', val_cindex, epoch)
+        writer.add_scalar('latest_test/c_index', test_cindex, epoch)
+        writer.close()
+    return results_val_dict, val_cindex, results_test_dict, test_cindex
+
+def loop_survival(
+        cur, epoch, model, loader, 
+        loss_fn=None, reg_fn=None, lambda_reg=0., writer=None, optimizer=None, gc=16, 
+        coattn=True, training=True, results_dir=None, 
+        early_stopping=None, return_summary=False
+    ): 
+    device=torch.device("cuda" if torch.cuda.is_available() else "cpu") 
+    model.train() if training else model.eval()
+    split_name = "Train" if training else "Validation"
+    loss_surv, running_loss = 0., 0.
+    if return_summary:
+        patient_results = {}
+        slide_ids = loader.dataset.slide_data['slide_id']
+
+    all_risk_scores = np.zeros((len(loader)))
+    all_censorships = np.zeros((len(loader)))
+    all_event_times = np.zeros((len(loader)))
+
+    for batch_idx, data in enumerate(loader):
+        
+        if coattn:
+            data_WSI, data_omic1, data_omic2, data_omic3, data_omic4, data_omic5, data_omic6, label, event_time, c = list(map(lambda x:x.to(device), data))
+            with torch.set_grad_enabled(training):
+                hazards, S, Y_hat, A  = model(x_path=data_WSI, x_omic1=data_omic1, x_omic2=data_omic2, x_omic3=data_omic3, x_omic4=data_omic4, x_omic5=data_omic5, x_omic6=data_omic6)
+        else:
+            data_WSI, data_omic, label, event_time, c = list(map(lambda x:x.to(device), data))
+            with torch.set_grad_enabled(training):
+                hazards, S, Y_hat, _, _ = model(x_path=data_WSI, x_omic=data_omic) # return hazards, S, Y_hat, A_raw, results_dict
+        
+        loss = loss_fn(hazards=hazards, S=S, Y=label, c=c)
+        loss_value = loss.item()
+
+        if reg_fn is None:
+            loss_reg = 0
+        else:
+            loss_reg = reg_fn(model) * lambda_reg
+
+        risk = -torch.sum(S, dim=1).detach().cpu().numpy()
+        all_risk_scores[batch_idx] = risk
+        all_censorships[batch_idx] = c.item()
+        all_event_times[batch_idx] = event_time
+
+        if return_summary:
+            slide_id = slide_ids.iloc[batch_idx]
+            patient_results.update({
+                slide_id: {
+                    'slide_id': np.array(slide_id), 
+                    'risk': risk, 
+                    'disc_label': label.item(), 
+                    'survival': np.asscalar(event_time), 
+                    'censorship': np.asscalar(c)
+            }})
+
+
+        loss_surv += loss_value
+        running_loss += loss_value + loss_reg
+
+        if (batch_idx + 1) % 100 == 0:
+            print('batch {}, loss: {:.4f}, label: {}, event_time: {:.4f}, risk: {:.4f}, bag_size: {}'.format(batch_idx, loss_value + loss_reg, label.item(), float(event_time), float(risk), data_WSI.size(0)))
+        
+        if training:
+            # backward pass
+            loss = loss / gc + loss_reg
+            loss.backward()
+
+            if (batch_idx + 1) % gc == 0: 
+                optimizer.step()
+                optimizer.zero_grad()
+
+    # calculate loss and error for epoch
+    loss_surv /= len(loader)
+    running_loss /= len(loader)
+
+    c_index = concordance_index_censored((1-all_censorships).astype(bool), all_event_times, all_risk_scores, tied_tol=1e-08)[0]
+    print('{} | epoch: {}, loss_surv: {:.4f}, loss: {:.4f}, train_c_index: {:.4f}'.format(split_name, epoch, loss_surv, running_loss, c_index))
+
+    if writer:
+        writer.add_scalar(f'{split_name}/loss_surv', loss_surv, epoch)
+        writer.add_scalar(f'{split_name}/loss', running_loss, epoch)
+        writer.add_scalar(f'{split_name}/c_index', c_index, epoch)
+
+    if early_stopping:
+        assert results_dir
+        early_stopping(epoch, loss_surv, model, ckpt_name=os.path.join(results_dir, "s_{}_minloss_checkpoint.pt".format(cur)))
+        
+        if early_stopping.early_stop:
+            print("Early stopping")
+            return True
+    return False
+
